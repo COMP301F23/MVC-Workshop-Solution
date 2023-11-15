21bb244bf6105744fb9bfba3dfb74f6c69c84db3
Grayson Clark <gclark@unc.edu>
2023-11-15 14:51:44 -0500

    Delete project files

--- src/main/com/comp301/tic-tac-toe/Controller/ClassicMvcController.java
+++
@@ -1,6 +0,0 @@
-package com.comp301.tic-tac-toe.controller;
-
-public interface ClassicMvcController {
-    /** Handles the click action to go ask loves me or loves me not */
-    void askButton();
-}
--- src/main/com/comp301/tic-tac-toe/Model/ModelObserver.java
+++
@@ -1,6 +0,0 @@
-package com.comp301.a00rameses.model;
-
-public interface ModelObserver {
-    /** When a model value is changed, the model calls update() on all active ModelObserver objects */
-    void update(Model model);
-}
